f46545f0c58557cfe92be25b724192210c6c16f2
tkcjesse <tkcjesse@gmail.com>
2015-09-06 01:30:28 -0500

    Update Config_Vehicles.hpp

--- Altis_Life.Altis/Config_Vehicles.hpp
+++ Altis_Life.Altis/Config_Vehicles.hpp
@@ -65,9 +65,10 @@ class CarShops {
 			{ "B_Quadbike_01_F", 2500, "" },
 			{ "B_G_Offroad_01_F", 15000, "" },
 			{ "O_MRAP_02_F", 150000, "rebel" },
+			{ "B_G_Offroad_01_armed_F", 750000, "rebel" },
 			{ "B_Heli_Light_01_F", 325000, "rebel" },
 			{ "I_Heli_Transport_02_F", 30000, "rebel" },
-			{ "B_G_Offroad_01_armed_F", 750000, "rebel" }
+			{ "I_Heli_light_03_unarmed_F", 75000, "rebel" }
 		};
 	};
 
